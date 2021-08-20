00ce0dd6de40616819a40ebb675473e11414e905
sspaeti <simu@sspaeti.com>
2021-08-20 15:49:07 +0200

    update dotfiles

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -4,7 +4,7 @@ cp ~/Library/ApplicationSupport/Code/User/settings.json $git/dotfiles/vscode/set
 cp ~/Library/ApplicationSupport/Code/User/keybindings.json $git/dotfiles/vscode/keybindings.json
 cp ~/.vimrc $git/dotfiles/vim/vimrc
 
-brew bundle dump > $git/dotfiles/Brewfilee -f
+brew bundle dump > $git/dotfiles/Brewfile -f
 
 source $venvs/dagster/bin/activate
 pip freeze > $git/dotfiles/python/venvs/dagster.txt
