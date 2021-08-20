e500a864c8bc26468007fb88eea20d48ce001804
sspaeti <simu@sspaeti.com>
2021-08-20 15:47:56 +0200

    update dotfiles automatically

---
+++ backup_dotfiles.sh
@@ -0,0 +1,11 @@
+#this script is done to automatically backup alls my dotsfiles
+
+cp ~/Library/ApplicationSupport/Code/User/settings.json $git/dotfiles/vscode/settings.json
+cp ~/Library/ApplicationSupport/Code/User/keybindings.json $git/dotfiles/vscode/keybindings.json
+cp ~/.vimrc $git/dotfiles/vim/vimrc
+
+brew bundle dump > $git/dotfiles/Brewfilee -f
+
+source $venvs/dagster/bin/activate
+pip freeze > $git/dotfiles/python/venvs/dagster.txt
+deactivate
