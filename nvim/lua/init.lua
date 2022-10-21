357765142bd8e8d1e66cc37a4ffd1307de163000
sspaeti <simu@sspaeti.com>
2022-10-21 20:36:37 +0200

    fix rust configs

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -4,25 +4,18 @@ vim.opt.list = true
 
 
 --rust
-local rt = {
-    server = {
-        settings = {
-            on_attach = function(_, bufnr)
-                -- Hover actions
-                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-                -- Code action groups
-                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-                require 'illuminate'.on_attach(client)
-            end,
-            ["rust-analyzer"] = {
-                checkOnSave = {
-                    command = "clippy"
-                }, 
-            },
-        }
-    },
-}
-require('rust-tools').setup(rt)
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
 
 
 -- LSP Diagnostics Options Setup 
