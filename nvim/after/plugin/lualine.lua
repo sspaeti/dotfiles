1ec3388df5abc92fae83a7de27ddf187dfb9c934
sspaeti <simu@sspaeti.com>
2023-02-21 22:12:33 +0100

    adding venv for python in status line

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,4 +1,3 @@
--- require('lualine').setup()
 
 local hide_in_width = function(width)
     return function()
@@ -51,6 +50,25 @@ local active_lsp = {
     icon = '',
     cond = function() return #vim.lsp.get_active_clients() > 0 and hide_in_width(100)() end
 }
+
+--get python virtual env
+function split(input, delimiter)
+    local arr = {}
+    string.gsub(input, '[^' .. delimiter ..']+', function(w) table.insert(arr, w) end)
+    return arr
+end
+
+local function get_venv()
+	local venv = vim.env.VIRTUAL_ENV
+	if venv then
+    local params = split(venv, '/')
+		return 'venv:'..params[table.getn(params)]..''
+
+	else
+		return ''
+	end
+end
+
 --word per minute
 local wpm = require("wpm")
 
@@ -67,7 +85,7 @@ require'lualine'.setup {
     lualine_a = {'mode'},
     lualine_b = {},
     lualine_c = {branch, active_lsp, filename},
-    lualine_x = {wpm.historic_graph, diff, diagnostics, filetype},
+    lualine_x = {wpm.historic_graph, diff, diagnostics, {get_venv, color={gui='bold'}}, filetype},
     lualine_y = {},
     lualine_z = {progress, location}
   },
