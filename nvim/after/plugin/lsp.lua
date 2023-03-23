c0c3def695315119a9b91da4fbc2da8b64b82902
sspaeti <simu@sspaeti.com>
2023-03-23 16:18:17 +0100

    update misc

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -99,9 +99,14 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "sC", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<Leader>lw", function() vim.lsp.buf.workspace_symbol() end, opts)
   -- vim.keymap.set("n", "<Leader>lt", function() vim.diagnostic.open_float() end, opts) --done with :TroubleToggle
+
   vim.keymap.set("n", "<Leader>ln", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]n", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "<Leader>lp", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "[p", function() vim.diagnostic.goto_prev() end, opts)
+
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>lh", function() vim.lsp.buf.signature_help() end, opts)
 
   -- turn on grammarly language server only for filetype=markdown
   if client.name == "grammarly" then
