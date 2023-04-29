73bff03f460aa3d9d1be4b13881c4f4c9e9b951a
sspaeti <simu@sspaeti.com>
2023-04-29 15:34:36 +0200

    configure mutt

---
+++ nvim/after/ftplugin/mail.lua
@@ -0,0 +1,7 @@
+-- This setting are for NeoMutt: https://seniormars.github.io/posts/neomutt/#motivation
+vim.opt_local.spell = true
+vim.opt_local.spelllang = 'en_us'
+vim.opt_local.fo:append('aw')
+local map = vim.api.nvim_buf_set_keymap
+local options = { noremap = true, silent = true}
+map(0, 'n', '<leader>x', 'ZZ', options)
