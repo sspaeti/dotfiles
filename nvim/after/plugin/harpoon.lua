e035d5c7f08a5ca5927e4830b8a0e04d9f8fccad
sspaeti <simu@sspaeti.com>
2023-05-22 15:03:53 +0200

    update shorcuts

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -12,3 +12,9 @@ vim.keymap.set("n", "<leader>hj", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>hk", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>hl", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>h;", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<leader>j", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>k", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>l", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>;", function() ui.nav_file(4) end)
+
