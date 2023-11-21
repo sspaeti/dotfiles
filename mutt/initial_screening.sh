f226843c8e744f30672c4200167fd9f52a16746c
sspaeti <simu@sspaeti.com>
2023-11-21 23:30:04 +0100

    working screener

--- mutt/initial_screening.sh
+++ mutt/initial_screening.sh
@@ -4,55 +4,54 @@
 screened_in="$HOME/.config/mutt/screened_in.txt"
 screened_out="$HOME/.config/mutt/screened_out.txt"
 
-
 ## trim trailing spaces
 sed -i '' 's/[[:space:]]*$//' "$screened_in"
 sed -i '' 's/[[:space:]]*$//' "$screened_out"
-
 
 # Function to check if an email sender is in a file
 in_file() {
     local sender=$1
     local file=$2
-    grep -Fxq "$sender" "$file" #debugging: && echo "Found $sender in $file" || echo "Not found in $file"
+    grep -Fxq "$sender" "$file"
 }
-# in_file() {
-#     local sender
-#     sender=$(echo $1 | xargs)
-#     local file=$2
-#     grep -Fxq "$sender" "$file"
-# }
-
 
 # Correct the path to your mail directory (remove the trailing /)
 mail_dir="$HOME/Documents/mutt/sspaeti.com"
 
-
-printf "Sender: %s\n" "$sender"
-printf "Screened-in Entry: %s\n" "$(head -n 1 "$HOME/.config/mutt/screened_in.txt"
-)"
-
-
 printf "Starting initial screening...\n"
 
-# Process each new email in the 'new' directory
-for email in $(find "$mail_dir" -maxdepth 3 -type f -path "*/new/*"); do
-    # print a spacer ------------------- with a new line at the end
-    printf -- "----------------------------------------\n"
-    # printf "Processing %s\n" "$email"
-    # Extract sender's email address
+# Process each new email in the 'new' directory and 'cur' directory of INBOX only
+for email in $(find "$mail_dir" -maxdepth 3 -type f \( -path "*/new/*" -o -path "*/INBOX/cur/*" \)); do
+#only new
+# for email in $(find "$mail_dir" -maxdepth 3 -type f -path "*/new/*"); do
     sender=$(grep "^From:" "$email" | head -1 | sed 's/.*<\(.*\)>/\1/')
-    printf "Sender: %s\n" "$sender"
+    # printf "Sender: %s\n" "$sender"
 
-    # Debugging print
-    # printf "Screened-in Entry: %s\n" "$(head -n 1 "$screened_in")"
-
-    # Check if sender is in screened lists
-    if ! in_file "$sender" "$screened_in" && ! in_file "$sender" "$screened_out"; then
-        # Move to ToScreen/new folder
-        # mv "$email" "$mail_dir/ToScreen/new/"
-        printf "!!!!!!!Moved to ToScreen/new --> %s\n" "$email"
+    if in_file "$sender" "$screened_in"; then
+        # printf "Not moved (Screened In): %s\n" "$email"
+        : # No operation, placeholder
+    elif in_file "$sender" "$screened_out"; then
+        printf -- "----------------------------------------\n"
+        printf "Sender: %s\n" "$sender"
+        printf "Moving to ScreenedOut/new: '%s'\n" "$email"
     else
-      printf "Not moved %s" "$email"
+        # printf "Else: Nothing should be done here: %s\n" "$email"
+        :
     fi
 done
+
+printf -- "####################################\n"
+printf "Starting moving ToScreen back if approved by now...\n"
+
+
+# Move emails from ToScreen/new back to INBOX/new if the sender is now in screened_in
+for email in $(find "$mail_dir/ToScreen/new" -type f); do
+    sender=$(grep "^From:" "$email" | head -1 | sed 's/.*<\(.*\)>/\1/')
+    
+    if in_file "$sender" "$screened_in"; then
+        printf -- "----------------------------------------\n"
+        mv "$email" "$mail_dir/INBOX/new/"
+        printf "Moved back to INBOX/new: %s\n" "$email"
+    fi
+done
+
