f003a6e236e372636490e470c2eddb914d58132f
sspaeti <simu@sspaeti.com>
2023-11-21 23:50:14 +0100

    going to bed now :)

--- mutt/launchctl/loading-them.sh
+++ mutt/launchctl/loading-them.sh
@@ -1,9 +1,11 @@
-cp ./offlineimap_sspaeti.plist ~/Library/LaunchAgents/
-cp ./initial_screening.plist ~/Library/LaunchAgents/
-
-# should be run manaully in mutt wth `S` (sync)
+## should be run manaully in mutt wth `S` (sync)
+# cp ./offlineimap_sspaeti.plist ~/Library/LaunchAgents/
 # launchctl load ~/Library/LaunchAgents/offlineimap_sspaeti.plist
 
+# Should run automatically, not that I have a long startup each time
+#
+launchctl unload ~/Library/LaunchAgents/initial_screening.plist
+rm ~/Library/LaunchAgents/initial_screening.plist
 
-#launchctl load ~/Library/LaunchAgents/initial_screening.plist
-
+cp ./initial_screening.plist ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/initial_screening.plist
