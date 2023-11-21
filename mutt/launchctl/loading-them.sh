5b9721583f1051f74a3f20256f4b1a65dfe528e3
sspaeti <simu@sspaeti.com>
2023-11-21 23:31:04 +0100

    working screener version Mutt

---
+++ mutt/launchctl/loading-them.sh
@@ -0,0 +1,9 @@
+cp ./offlineimap_sspaeti.plist ~/Library/LaunchAgents/
+cp ./initial_screening.plist ~/Library/LaunchAgents/
+
+# should be run manaully in mutt wth `S` (sync)
+# launchctl load ~/Library/LaunchAgents/offlineimap_sspaeti.plist
+
+
+#launchctl load ~/Library/LaunchAgents/initial_screening.plist
+
