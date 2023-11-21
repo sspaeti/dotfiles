b2a34248e8c16a94f44bcd66007395f90b231886
sspaeti <simu@sspaeti.com>
2023-11-21 18:58:43 +0100

    update screener

--- mutt/initial_screening.sh
+++ mutt/initial_screening.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#
-# TODO
-# `mail_dir` needs to be updated to local file path. As I'm using IMAP as mail_dir, I'd need to use local mail location and sync to IMAP with sync or similar.
-# see more info on obsidian://open?vault=SecondBrain&file=%E2%9A%9B%EF%B8%8F%20Areas%2F%F0%9F%93%AC%20Inbox%2FScreener%20in%20(Neo)Mutt
-
-
-#checks if the sender of a new email is in screened_in.txt or screen_out.txt. If not, the email is moved to =ToScreen.
-#This script will check each new email in the Inbox and move it to =ToScreen if the sender is not listed in screened_in.txt or screened_out.txt. 
-#Note that the specific implementation can vary depending on your mail server setup and how you access your emails (IMAP, local mailbox, etc.).
-#
-#
 
 # Paths to screened lists
 screened_in="$HOME/.config/mutt/screened_in.txt"
@@ -23,7 +12,7 @@ in_file() {
 }
 
 # Path to your mail directory
-mail_dir="~/Documents/mutt/Mail/Inbox"
+mail_dir="~/Documents/mutt/sspaeti.com"
 
 # Process each new email
 for email in $(find $mail_dir/new -type f); do
@@ -33,7 +22,7 @@ for email in $(find $mail_dir/new -type f); do
     # Check if sender is in screened lists
     if ! in_file "$sender" "$screened_in" && ! in_file "$sender" "$screened_out"; then
         # Move to ToScreen folder (adjust command based on your mail setup)
-        mv "$email" "~/Mail/ToScreen/"
+        mv "$email" "~/Documents/mutt/ToScreen/"
     fi
 done
 
