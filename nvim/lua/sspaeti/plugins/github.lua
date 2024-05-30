16b4bf0db9a357ccf3c0290652fdc8dd2076640b
sspaeti <simon@ssp.sh>
2024-05-30 15:40:59 +0200

    change gh.nvim with octo.nvim to navigate github

--- nvim/lua/sspaeti/plugins/github.lua
+++ nvim/lua/sspaeti/plugins/github.lua
@@ -1,15 +1,40 @@
 return {
-  "ldelossa/gh.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    {
-      "ldelossa/litee.nvim",
-      config = function()
-        require("litee.lib").setup()
-      end,
+  --work on a PR from GitHub within Neovim: Octo works better now than gh.nvim
+  --{
+  --"ldelossa/gh.nvim",
+  --event = "VeryLazy",
+  --dependencies = {
+  --  {
+  --    "ldelossa/litee.nvim",
+  --    config = function()
+  --      require("litee.lib").setup()
+  --    end,
+  --  },
+  --},
+  --config = function()
+  --  require("litee.gh").setup()
+  --end,
+  --},
+  {
+    'pwntester/octo.nvim',
+    cmd = { 'Octo' }, -- only run when Octo is called (also to avoid seeing warning: `Cannot request projects v2, missing scope 'read:project'` all the time)
+
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- OR 'ibhagwan/fzf-lua',
+      'nvim-tree/nvim-web-devicons',
     },
-  },
-  config = function()
-    require("litee.gh").setup()
-  end,
+    config = function()
+      require "octo".setup({
+        {
+          --TODO: does not work...
+          suppress_missing_scope = {
+            projects_v2 = true,
+          }
+        }
+
+      })
+    end
+  }
 }
