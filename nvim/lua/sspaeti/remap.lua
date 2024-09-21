7bc37fdf260e47499a6aee4f29abe84630b35b21
sspaeti <simon@ssp.sh>
2024-09-21 11:45:48 +0200

    add handy shorcuts

--- nvim/lua/sspaeti/remap.lua
+++ nvim/lua/sspaeti/remap.lua
@@ -61,6 +61,10 @@ vim.keymap.set("n", "S", ":%s//g<Left><Left>") --used for lazy.
 vim.keymap.set("n", "<leader>s", ":%s//g<Left><Left>")
 vim.keymap.set("n", "<leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
+--ciq (change in quotes) 
+vim.keymap.set("n", "ciq", "ci\"")
+--cib (change in brackets)
+vim.keymap.set("n", "cib", "ci(")
 
 --improve cmd line completion (: mode or wildmenu) - allow c-j and c-n to accept completion (tab)
 -- Map Ctrl+n to wildmenu completion in command mode
