b748ff2586d4a138a23196594af915685db67d39
sspaeti <simon@ssp.sh>
2024-10-04 10:19:18 +0200

    fix after upgrade

--- nvim/lua/sspaeti/plugins/_init_lazy.lua
+++ nvim/lua/sspaeti/plugins/_init_lazy.lua
@@ -113,10 +113,6 @@ return {
       require("highlight-undo").setup({
         hlgroup = "HighlightUndo",
         duration = 600,
-        keymaps = {
-          { "n", "u",     "undo", {} },
-          { "n", "<C-r>", "redo", {} },
-        },
       })
     end,
   },
