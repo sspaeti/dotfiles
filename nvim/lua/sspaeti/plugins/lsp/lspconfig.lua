a044a05c79e97f099aa136e32747955505a5e546
sspaeti <simon@ssp.sh>
2024-10-09 13:39:27 +0200

    update tsserver

--- nvim/lua/sspaeti/plugins/lsp/lspconfig.lua
+++ nvim/lua/sspaeti/plugins/lsp/lspconfig.lua
@@ -139,7 +139,7 @@ return {
       })
 
       -- configure typescript server with plugin
-      lspconfig["tsserver"].setup({
+      lspconfig["ts_ls"].setup({
         capabilities = capabilities,
         on_attach = on_attach,
       })
--- nvim/lua/sspaeti/plugins/lsp/mason.lua
+++ nvim/lua/sspaeti/plugins/lsp/mason.lua
@@ -28,7 +28,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        -- "tsserver",
         "html",
         "cssls",
         "tailwindcss",
