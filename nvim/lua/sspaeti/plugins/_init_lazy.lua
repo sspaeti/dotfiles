686fe4ea31f9ec08acbdd1ac648eed15b7cfb5f6
sspaeti <simon@ssp.sh>
2024-10-20 15:37:41 +0200

    update

--- nvim/lua/sspaeti/plugins/_init_lazy.lua
+++ nvim/lua/sspaeti/plugins/_init_lazy.lua
@@ -83,9 +83,9 @@ return {
       })
     end,
   },
-  { "mhinz/vim-signify",     event = "VeryLazy" },          --highlighing changes not commited to last commmit
+  { "mhinz/vim-signify",     event = "VeryLazy" }, --highlighing changes not commited to last commmit
 
-  { "APZelos/blamer.nvim",   event = "VeryLazy" },          --gitlens blame style',
+  { "APZelos/blamer.nvim",   event = "VeryLazy" }, --gitlens blame style',
   -- 'nvim-lua/popup.nvim',
   { "nvim-lua/plenary.nvim", event = "VeryLazy" },
   -- search
