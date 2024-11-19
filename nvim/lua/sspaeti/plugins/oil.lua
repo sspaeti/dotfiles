4dfe5740120f555fb83b7232d98d0ad631b5600b
sspaeti <simon@ssp.sh>
2024-11-19 23:13:07 +0100

    using yazi

--- nvim/lua/sspaeti/plugins/oil.lua
+++ nvim/lua/sspaeti/plugins/oil.lua
@@ -1,28 +1,29 @@
-return {
-  'stevearc/oil.nvim',
-  event = "VeryLazy",
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup {
-      columns = { "icon" },
-      keymaps = {
-        ["<C-h>"] = false,
-        ["<C-l>"] = false,
-        ["<C-j>"] = false,
-        ["<C-k>"] = false,
-      },
-      view_options = {
-        show_hidden = true,
-      },
-      delete_to_trash = true,
-    }
+return {}
+-- return {
+--   'stevearc/oil.nvim',
+--   event = "VeryLazy",
+--   opts = {},
+--   -- Optional dependencies
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   config = function()
+--     require("oil").setup {
+--       columns = { "icon" },
+--       keymaps = {
+--         ["<C-h>"] = false,
+--         ["<C-l>"] = false,
+--         ["<C-j>"] = false,
+--         ["<C-k>"] = false,
+--       },
+--       view_options = {
+--         show_hidden = true,
+--       },
+--       delete_to_trash = true,
+--     }
 
-    -- Open parent directory in current window
-    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+--     -- Open parent directory in current window
+--     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-    -- Open parent directory in floating window
-    vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directory in floating window" })
-  end,
-}
+--     -- Open parent directory in floating window
+--     vim.keymap.set("n", "<leader>-", require("oil").toggle_float, { desc = "Open parent directory in floating window" })
+--   end,
+-- }
