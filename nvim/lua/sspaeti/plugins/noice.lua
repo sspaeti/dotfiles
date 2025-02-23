2ace74c35f23a0c99145f82ea25c0fd596cc6059
sspaeti <simon@ssp.sh>
2025-02-23 08:53:23 +0100

    ignore annoying popup on neo-tree when renaming or searching

--- nvim/lua/sspaeti/plugins/noice.lua
+++ nvim/lua/sspaeti/plugins/noice.lua
@@ -10,6 +10,20 @@ return {
       -- NOTE: If you enable messages, then the cmdline is enabled automatically.
       -- This is a current Neovim limitation.
       view = "mini", --show default operation in lover right corner. Warn/Errors are still as popu (notify). options: notify, split, vsplit, popup, mini, cmdline, cmdline_popup, cmdline_output, messages, confirm, hover, popupmenu
+    },
+    routes = {
+      { --this turns off notification in neo-tree that are distracting
+        view = 'mini',
+        filter = {
+          event = 'notify',
+          any = {
+            { find = 'hidden' },
+            { find = 'clipboard' },
+            { find = 'Deleted' },
+            { find = 'Renamed' },
+          },
+        },
+      }
     }
 
   },
