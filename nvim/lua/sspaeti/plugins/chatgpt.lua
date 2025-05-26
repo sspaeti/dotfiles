df396c655f47e88775ea2b48cd1540d51a9776a1
sspaeti <simon@ssp.sh>
2025-05-26 15:17:00 +0200

    adding mcphub

--- nvim/lua/sspaeti/plugins/chatgpt.lua
+++ nvim/lua/sspaeti/plugins/chatgpt.lua
@@ -38,12 +38,38 @@ return {
         ---Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
         provider = "claude",
         hints = { enabled = false },
+        -- for MCPhub
+        -- system_prompt as function ensures LLM always has latest MCP server state
+        -- This is evaluated for every message, even in existing chats
+        system_prompt = function()
+          local hub = require("mcphub").get_hub_instance()
+          return hub and hub:get_active_servers_prompt() or ""
+        end,
+        -- Using function prevents requiring mcphub before it's loaded
+        custom_tools = function()
+          return {
+            require("mcphub.extensions.avante").mcp_tool(),
+          }
+        end,
       })
     end,
     -- keys = {
     --   { "<leader>ai", "<cmd>AvanteAsk<cr>", desc = "Open AI/Claude on this file" },
     -- }
   },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup()
+    end
+  },
+
+
+  --
   {
     "github/copilot.vim",
     event = "VeryLazy",
