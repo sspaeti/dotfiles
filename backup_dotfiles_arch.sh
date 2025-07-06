ee83ca4f1a6b95775985d2234f680e46b19eb7e0
Simon Spati <simon@ssp.sh>
2025-07-06 23:55:39 +0200

    update

--- backup_dotfiles_arch.sh
+++ backup_dotfiles_arch.sh
@@ -6,3 +6,11 @@ cp -r ~/.local/share/omarchy/default/hypr/* _arch-linux/hypr/omarchy/default
 #keyboard shortcuts
 cp /etc/kanata/kanata.kbd _arch-linux/kanata/
 cp /etc/systemd/system/kanata.service _arch-linux/kanata/
+
+cp -r ~/.config/waybar/ _arch-linux/waybar/
+cp -r ~/.config/wofi/ _arch-linux/wofi
+
+
+
+#! I also have the mac ghostty settings in the dots files one level above
+cp -r ~/.config/ghostty/ _arch-linux/ghostty
