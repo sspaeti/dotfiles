a03529abaf21257afd51f73ab3e3627d567b11f9
Simon Spati <simon@ssp.sh>
2025-07-06 20:38:37 +0200

    add arch-linux dots

--- backup_dotfiles_arch.sh
+++ backup_dotfiles_arch.sh
@@ -1,7 +1,7 @@
 #this script is done to automatically backup alls my dotsfiles for arch that are not part of backup_dotfiles.sh
 
-cp -r ~/.config/hypr/ _arch-linux/hypr/
-cp -r ~/.local/share/omarchy/ _arch-linux/hypr/omarchy/
+cp -r ~/.config/hypr/ _arch-linux/
+cp -r ~/.local/share/omarchy/default/hypr/* _arch-linux/hypr/omarchy/default
 
 #keyboard shortcuts
 cp /etc/kanata/kanata.kbd _arch-linux/kanata/
