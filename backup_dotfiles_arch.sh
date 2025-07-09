66d307ed34b2bfe2bbc9a84fb751b0c46da68023
Simon Spati <simon@ssp.sh>
2025-07-09 13:28:06 +0200

    update kanata

--- backup_dotfiles_arch.sh
+++ backup_dotfiles_arch.sh
@@ -5,8 +5,12 @@ cp -r ~/.local/share/omarchy/default/hypr/* _arch-linux/hypr/omarchy/default
 cp -r ~/.local/share/omarchy/bin/*  _arch-linux/hypr/omarchy/bin
 
 #keyboard shortcuts
-cp /etc/kanata/* _arch-linux/kanata/
+cp /etc/kanata/lenovo.kbd _arch-linux/kanata/
+cp /etc/kanata/kinesis.kbd _arch-linux/kanata/
 cp /etc/systemd/system/kanata.service _arch-linux/kanata/
+cp ~/.local/bin/switch-keyboard _arch-linux/bin/helper/
+# will be set automatically with above switch-keyboard bash script
+# cp /etc/kanata/kanata.kbd _arch-linux/kanata/
 
 cp -r ~/.config/waybar/ _arch-linux/waybar/
 cp -r ~/.config/wofi/ _arch-linux/wofi
@@ -14,3 +18,5 @@ cp -r ~/.config/wofi/ _arch-linux/wofi
 
 #! I also have the mac ghostty settings in the dots files one level above
 cp -r ~/.config/ghostty/ _arch-linux/ghostty
+
+
