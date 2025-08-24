c8d0670742363122a786be71d8bfabe9b7cbadcc
sspaeti <simon@ssp.sh>
2025-08-24 17:49:33 +0200

    show hidden files always

--- nvim/.config/nvim/lua/sspaeti/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/sspaeti/plugins/neo-tree.lua
@@ -16,10 +16,16 @@ return {
       position = "right",
       filesystem = {
         follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every time
+          enabled = true,         -- This will find and focus the file in the active buffer every time
           --              -- the current file is changed while the tree is open.
-          leave_dirs_open = true,   -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
+        filtered_items = {
+          visible = true, -- If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+        never_show = { ".git" },
       },
       window = {
         mappings = {
