d84cb7c7183ca4cd8611051646a79e744924e846
sspaeti <simon@ssp.sh>
2025-09-13 21:00:05 +0200

    add SQL-Runner to query DBs

--- nvim/.config/nvim/lua/sspaeti/plugins_custom/init.lua
+++ nvim/.config/nvim/lua/sspaeti/plugins_custom/init.lua
@@ -2,3 +2,6 @@ require("sspaeti.plugins_custom.todo_float").setup({
 	target_file = "todo.md",
 	global_file = "~/Simon/SecondBrain/🌿 Projects/My Todos.md",
 })
+
+
+require("sspaeti.plugins_custom.sql_runner")
