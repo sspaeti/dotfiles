d74791ed7a1680447fddac8b9f808a28d1fc78b4
sspaeti <simon@ssp.sh>
2026-05-20 11:15:45 +0200

    zoxide and yazi update

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,2 +1,12 @@
 -- DuckDB plugin configuration
 require("duckdb"):setup()
+
+--adding cd'ed in directories to zoxied, so I can easily find them with `Z` inside yazi or zoxide
+ps.sub("cd", function()
+  local cwd = tostring(cx.active.current.cwd)
+  ya.emit("shell", {
+    "zoxide add -- " .. ya.quote(cwd) .. " >/dev/null 2>&1",
+    orphan = true,
+    confirm = true,
+  })
+end)
