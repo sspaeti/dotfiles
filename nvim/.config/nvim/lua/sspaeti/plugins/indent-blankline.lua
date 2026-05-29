6933a258cb2df635fda87eb86ed7e04507464fe5
sspaeti <simon@ssp.sh>
2026-05-29 09:15:44 +0200

    light theme

--- nvim/.config/nvim/lua/sspaeti/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/sspaeti/plugins/indent-blankline.lua
@@ -3,8 +3,21 @@ return {
   event = "VeryLazy",
   main = "ibl",
   config = function()
-    vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1F1F28 gui=nocombine]]
-    vim.cmd [[highlight IndentBlanklineIndent2 guibg=#252535 gui=nocombine]]
+    local function set_ibl_hl()
+      local c1, c2
+      if vim.o.background == "light" then
+        c1, c2 = "#DCD7AA", "#C8CCAA"
+      else
+        c1, c2 = "#1F1F28", "#252535"
+      end
+      vim.api.nvim_set_hl(0, "IndentBlanklineIndent1", { bg = c1, default = true })
+      vim.api.nvim_set_hl(0, "IndentBlanklineIndent2", { bg = c2, default = true })
+    end
+    set_ibl_hl()
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      group = vim.api.nvim_create_augroup("IblCustomHighlights", { clear = true }),
+      callback = set_ibl_hl,
+    })
     require("ibl").setup({
       indent = { char = " " },
       whitespace = {
